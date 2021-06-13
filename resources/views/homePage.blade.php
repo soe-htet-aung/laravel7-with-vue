f49293440eea1158a32d02fc9074f6f0d3e3acfe
Thu Zar Htwe <scm.thuzarhtwe@gmail.com>
2021-06-13 21:14:37 +0630

    [sidebar]_Initial commit for sidebar

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>YBS</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>
+    <body>
+        <!-- Below code will call the vue script -->
+        <div id="app"><sidebar-component></sidebar-component></div>
+        <link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
