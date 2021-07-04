97714204ad0a5b1e215e24325265ecb897b02abc
MyatMon <scm.myatmon@gmail.com>
2021-07-04 21:47:37 +0630

    [modify_tables]_migration and seeder with csv for bus table

--- database/seeds/BusSeeder.php
+++ database/seeds/BusSeeder.php
@@ -12,12 +12,28 @@ class BusSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i < 6; $i++) { 
-	    	Bus::create([
-	            'bus_name' => $i,
-	            'price' => 20000 * $i,
-	            'bus_line' => Str::random(25),
-	        ]);
-    	}
+        $skipFirstRow = true;
+        if (($handle = fopen ( storage_path('Seeder/') . 'Bus.csv', 'r' )) !== FALSE) {
+            while ( ($data = fgetcsv ( $handle, 1000, ',' )) !== FALSE ) {
+                if ($skipFirstRow) { $skipFirstRow = false; continue; }
+                if (!mb_check_encoding($data, 'UTF-8')) {
+                    $data = mb_convert_encoding($data, "UTF-8", "auto");
+                }
+                $csv_data[] =[
+                    'bus_id' => $data [0],
+                    'bus_name' => $data [1],
+                    'price' => $data [2],
+                    'bus_line' => $data [3],
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now(),
+                ];
+            }
+            if (isset($csv_data)) {
+                foreach (array_chunk($csv_data, 1000) as $data)  {
+                    Bus::insert($data);
+                }
+            }
+            fclose ( $handle );
+        }
     }
 }
