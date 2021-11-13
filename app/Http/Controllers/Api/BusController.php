298fe30402c12eebee4ac5d5da064326501ff6ee
SeinMohMohZin <scm.seinmohmohzin@gmail.com>
2021-11-13 19:16:58 +0630

    sidebar [api]_api for YBS master data upload

--- app/Http/Controllers/Api/BusController.php
+++ app/Http/Controllers/Api/BusController.php
@@ -31,4 +31,61 @@ class BusController extends Controller {
         $busStops = BusStop::where('bus_id', $busId)->get();
         return $busStops;
     }
+
+    /**
+     * upload YBS data using csv
+     * 
+     */
+    public function updateBusStop() {
+        DB::table('bus')->delete();
+        $skipFirstRow = true;
+        if (($handle = fopen ( storage_path('Seeder/') . 'Bus.csv', 'r' )) !== FALSE) {
+            while ( ($data = fgetcsv ( $handle, 1000, ',' )) !== FALSE ) {
+                if ($skipFirstRow) { $skipFirstRow = false; continue; }
+                if (!mb_check_encoding($data, 'UTF-8')) {
+                    $data = mb_convert_encoding($data, "UTF-8", "auto");
+                }
+                $csv_data[] =[
+                    'bus_id' => $data [0],
+                    'bus_name' => $data [1],
+                    'price' => $data [2],
+                    'bus_line' => $data [3],
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now(),
+                ];
+            }
+            if (isset($csv_data)) {
+                foreach (array_chunk($csv_data, 1000) as $data)  {
+                    Bus::insert($data);
+                }
+            }
+            fclose ( $handle );
+        }
+
+        DB::table('bus_stop')->delete();
+        $skipFirstRow = true;
+        if (($handle = fopen ( storage_path('Seeder/') . 'BusStop.csv', 'r' )) !== FALSE) {
+            while ( ($data = fgetcsv ( $handle, 1000, ',' )) !== FALSE ) {
+                if ($skipFirstRow) { $skipFirstRow = false; continue; }
+                if (!mb_check_encoding($data, 'UTF-8')) {
+                    $data = mb_convert_encoding($data, "UTF-8", "auto");
+                }
+                $csv_datas[] =[
+                    'stop_id' => $data [0],
+                    'stop_name' => $data [1],
+                    'bus_id' => $data [2],
+                    'stop_latitude' => $data [3],
+                    'stop_longitude' => $data [4],
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now(),
+                ];
+            }
+            if (isset($csv_datas)) {
+                foreach (array_chunk($csv_datas, 1000) as $data)  {
+                    BusStop::insert($data);
+                }
+            }
+            fclose ( $handle );
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('getBus', 'Api\BusController@getBus')->name('getBus');  // get bus information
 
 Route::get('getBusStop/{busId}', 'Api\BusController@getBusStops')->name('getBusStops');  // get bus stop information
+
+Route::get('/UpdateBusStop', 'UpdateBusStopController@updateBusStop');  // YBS data upload api
